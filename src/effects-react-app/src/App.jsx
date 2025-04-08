2382b66dc34682acd254dacd3200b9c4520293eb
Lee-sung-il <wj132612@naver.com>
2025-04-08 10:46:09 +0900

    💄 Effect 소개 와 의존성
    API에서 초기 값 가져오는 법

---
+++ src/effects-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Counter from "./Counter.jsx";
+import QuoteFetcher from "./QuoteFetcher.jsx";
+import QuoteFetcherLoader from "./QuoteFetcherLoader.jsx";
+import ProfileViewerWithSearch from "./ProfileViewerWithSearch.jsx";
+
+function App() {
+  return (
+    <>
+      {/*<Counter/>*/}
+      {/*  <QuoteFetcherLoader/>*/}
+        <ProfileViewerWithSearch />
+    </>
+  )
+}
+
+export default App
